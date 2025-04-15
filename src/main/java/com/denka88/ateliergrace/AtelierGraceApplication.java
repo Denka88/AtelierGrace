2f11acb0f722725323e14808d44f24a7de8a5082
Denka88 <animedenisik.com@mail.ru>
2025-04-15 22:21:10 +0300

    init

---
+++ src/main/java/com/denka88/ateliergrace/AtelierGraceApplication.java
@@ -0,0 +1,13 @@
+package com.denka88.ateliergrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierGraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtelierGraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/denka88/ateliergrace/AtelierGraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denka88.ateliergrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierGraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
