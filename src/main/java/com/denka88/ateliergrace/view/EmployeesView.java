bb3b33081e206a139a95433cf2ce68b960677fa9
Denka88 <animedenisik.com@mail.ru>
2025-04-29 15:40:05 +0300

    employee update created

--- src/main/java/com/denka88/ateliergrace/view/EmployeesView.java
+++ src/main/java/com/denka88/ateliergrace/view/EmployeesView.java
@@ -2,16 +2,20 @@ package com.denka88.ateliergrace.view;
 
 import com.denka88.ateliergrace.MainLayout;
 import com.denka88.ateliergrace.model.Employee;
+import com.denka88.ateliergrace.model.Material;
 import com.denka88.ateliergrace.model.UserType;
 import com.denka88.ateliergrace.service.CurrentUserService;
 import com.denka88.ateliergrace.service.EmployeeService;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.contextmenu.GridContextMenu;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -28,6 +32,14 @@ public class EmployeesView extends VerticalLayout {
     private final EmployeeService employeeService;
     private final Grid<Employee> grid;
     private final CurrentUserService currentUserService;
+    
+    private FormLayout editForm = new FormLayout();
+    private TextField id = new TextField("ID");
+    private TextField editSurname = new TextField("Фамилия");
+    private TextField editName = new TextField("Имя");
+    private TextField editPatronymic = new TextField("Отчество");
+    private TextField editPost = new TextField("Должность");
+    private Button editButton = new Button("Сохранить изменения");
 
     public EmployeesView(EmployeeService employeeService, CurrentUserService currentUserService) {
         this.employeeService = employeeService;
@@ -42,7 +54,33 @@ public class EmployeesView extends VerticalLayout {
             UI.getCurrent().navigate("/employee-registration");
         });
 
-        add(grid, addEmployee);
+        editForm.setWidth("400px");
+        
+        id.setVisible(false);
+        
+        editButton.addClickListener(e->{
+            Employee updateEmployee = employeeService.findById(Long.valueOf(id.getValue())).orElse(null);
+            updateEmployee.setSurname(editSurname.getValue());
+            updateEmployee.setName(editName.getValue());
+            updateEmployee.setPatronymic(editPatronymic.getValue());
+            updateEmployee.setPost(editPost.getValue());
+            employeeService.update(updateEmployee);
+            updateGrid();
+            editForm.setVisible(false);
+        });
+        
+        grid.addCellFocusListener(e->{
+            id.setValue(String.valueOf(e.getItem().map(Employee::getId).orElse(null)));
+            editSurname.setValue(e.getItem().map(Employee::getSurname).orElse("Не доступно"));
+            editName.setValue(e.getItem().map(Employee::getName).orElse("Не доступно"));
+            editPatronymic.setValue(e.getItem().map(Employee::getPatronymic).orElse("Не доступно"));
+            editPost.setValue(e.getItem().map(Employee::getPost).orElse("Не доступно"));
+        });
+        
+        editForm.add(id, editSurname, editName, editPatronymic, editPost, editButton);
+        editForm.setVisible(false);
+        
+        add(grid, addEmployee, editForm);
     }
 
     private void setupGrid(){
@@ -53,17 +91,25 @@ public class EmployeesView extends VerticalLayout {
         grid.addColumn(Employee::getName).setHeader("Имя").setSortable(true).setAutoWidth(true);
         grid.addColumn(Employee::getPatronymic).setHeader("Отчество").setSortable(true).setAutoWidth(true);
         grid.addColumn(Employee::getPost).setHeader("Должность").setSortable(true).setAutoWidth(true);
-        if(currentUserService.getCurrentUserType().equals(UserType.ADMIN)){
-            grid.addColumn(new ComponentRenderer<>(Button::new, (button, employee) -> {
-                button.addThemeVariants(ButtonVariant.LUMO_ICON,
-                        ButtonVariant.LUMO_ERROR,
-                        ButtonVariant.LUMO_TERTIARY);
-                button.addClickListener(e -> {
-                    employeeService.delete(employee.getId());
-                    updateGrid();
-                });
-                button.setIcon(new Icon(VaadinIcon.TRASH));
-            })).setHeader("Действие").setAutoWidth(true);
+
+        GridContextMenu<Employee> contextMenu = grid.addContextMenu();
+        contextMenu.addItem("Изменить", e->{
+            if(editForm.isVisible()){
+                editForm.setVisible(false);
+            }else if(!editForm.isVisible()){
+                editForm.setVisible(true);
+                id.setValue(String.valueOf(e.getItem().map(Employee::getId).orElse(null)));
+                editSurname.setValue(e.getItem().map(Employee::getSurname).orElse("Не доступно"));
+                editName.setValue(e.getItem().map(Employee::getName).orElse("Не доступно"));
+                editPatronymic.setValue(e.getItem().map(Employee::getPatronymic).orElse("Не доступно"));
+                editPost.setValue(e.getItem().map(Employee::getPost).orElse("Не доступно"));
+            }
+        });
+        if(currentUserService.getCurrentUserType().equals(UserType.ADMIN)) {
+            contextMenu.addItem("Удалить", e -> {
+                employeeService.delete(e.getItem().map(Employee::getId).orElse(null));
+                updateGrid();
+            });
         }
     }
 
